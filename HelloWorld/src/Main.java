179dbb287d6ecba66dd2c81a7b99f950baf5935a
nua1030 <gumdong1030@gmail.com>
2025-03-25 11:39:59 +0900

    hee

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //1. 문자열로 이름선언
+        String name = "정제나";
+        //2. 정수형으로 나이선언
+        int age = 21;
+        System.out.println(name + "님은 " + age + "살입니다.");
+    }
+}
