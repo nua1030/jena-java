72c319f488c2f65b448461ca9c2c146242ed93fb
nua1030 <gumdong1030@gmail.com>
2025-04-15 11:31:46 +0900

    qw

---
+++ GradeCalculator/src/GradeCalculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("점수를 입력하세요: ");
+        int score = sc.nextInt();
+        String grade;
+
+        if (score >= 90) {
+            grade = "A";
+        }
+        else if (score >= 80) {
+            grade = "B";
+        }
+        else if (score >= 70) {
+            grade = "C";
+        }
+        else if (score >= 60) {
+            grade = "D";
+        }
+        else if (score >= 0) {
+            grade = "F";
+        }
+        else {
+            System.out.println("ERROR");
+            return;
+        }
+        System.out.println("학점:"+grade);
+
+    }
+}
